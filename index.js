7ddb4ed90f80fb22a55e905dabaeaf6a58eb4f8d
manojmogal007 <manojmogal07@gmail.com>
2023-10-15 10:52:08 +0530

    update navbar

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const burger = document.querySelector('.burger');
-    const menu = document.querySelector('.menu');
-
-    burger.addEventListener('click', function() {
-        menu.classList.toggle('active');
-    });
-});
